c15f45709cfd1ef5970843d9bb747b30dd64bb3f
chaninkou <michaelkoulove@yahoo.com>
2019-10-08 23:31:56 -0700

    Great problem. Get container with most water with two pointers O(n).

---
+++ src/mostWater/Main.java
@@ -0,0 +1,15 @@
+package mostWater;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] height = {1,8,6,2,5,4,8,3,7};
+		
+		System.out.println("Height: " + Arrays.toString(height));
+		
+		FindContainerWithMostWaterFunction solution = new FindContainerWithMostWaterFunction();
+		
+		System.out.println("Container with most water: " + solution.maxArea(height));
+	}
+}
